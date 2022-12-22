a13788e5a8f30d372fafc02b0492775927f32ae6
Jorge Castillo <jorge@wilqo.com>
2022-12-22 12:46:45 -0800

    create skeleton

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
 import './App.css';
-import {  useAppContext } from './AppContext'
+
 
 function App() {
-
-const {
-  data,
-  setQuery,
-  query
-} = useAppContext();
-
-console.log(data)
 
   return (
     <div className='flex items-center justify-center h-screen'>
       <div className="App drop-shadow-2xl bg-slate-100 w-[500px] h-[550px] p-6">
-        <input className='p-3 my-5 border w-full border-blue-200 rounded-3xl' type="text" value={query} onChange={(e) => setQuery(e.currentTarget.value)} placeholder="Search..."/>
-        {
-          data ? (
-            <>
-              <div className="flex items-center justify-center">
-                <img className='h-auto w-52' src={data.sprites.other.dream_world.front_default} alt=""/>
-              </div>
-              <div className='flex items-end justify-between mt-8'>
-                <p className='font-bold'>Name:</p>
-                <p className='text-blue-600 font-semibold uppercase'>{data.name}</p>
-              </div>
-              <div className='flex items-end justify-between mt-3'>
-                <p className='font-bold'>Abilities:</p>
-                <p className='text-blue-600 font-semibold'>
-                    {
-                      data.abilities.map((ability, index) => {
-                        return (
-                          <span>{`${ability.ability.name} ${data.abilities.length - 1 === index ? '': ','} `}</span>
-                        )
-                      })
-                    }
-                </p>
-            </div>
-            </>
-          ) : <p className='font-bold uppercase text-red-500 text-2xl mt-8'>No Pokemon found</p>
-        }
+          {/* Component Here */}
       </div>
     </div>
   );
--- src/AppContext.js
+++ src/AppContext.js
@@ -5,27 +5,10 @@ const AppContext = createContext();
 
 const AppProvider = ({children}) => {
 
-  const [data, setData] = useState(undefined);
-  const [query, setQuery] = useState('');
-
-  const getData = useCallback(async() => {
-    // if(query !== ''){
-    //   const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
-    //   const response = await apiResponse.json()
-    //   setData(response)
-    // }
-  }, [query])
-
-  useEffect(() => {
-    getData()
-  }, [getData]);
 
   return (
     <AppContext.Provider
-      value={{
-        data,
-        setQuery
-      }}
+      value={{}}
     >
       {children}
     </AppContext.Provider>
